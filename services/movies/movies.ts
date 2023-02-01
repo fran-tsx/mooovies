392c89ba6ffe13794e403231dc63d5dd0fb43640
Francisco Rodriguez <fjrodriguez.353@icloud.com>
2023-02-01 05:14:11 -0300

    feat: services updated and typed

--- services/movies/movies.ts
+++ services/movies/movies.ts
@@ -1,28 +1,45 @@
-import { STGetMoviesParams } from './movies.types'
+import {
+  DTMovie,
+  STFavoritesReturn,
+  STGetParams,
+  STSearchReturn,
+} from './movies.types'
 
-const favorites = async () => {
+const favorites = async (): Promise<STFavoritesReturn> => {
   const favoriteSeries = [
     {
-      imdbId: 'tt4158110',
-      title: 'Mr. Robot',
+      imdbId: 'tt0993846',
+      title: 'The Wolf of Wall Street',
     },
     {
-      imdbId: 'tt2543312',
-      title: 'Halt and Catch Fire',
+      imdbId: 'tt4154756',
+      title: 'Avengers: Infinity War',
+    },
+    {
+      imdbId: 'tt0816692',
+      title: 'Interstellar',
     },
   ]
-  const requests = favoriteSeries.map(({ imdbId }) => search({ imdbId }))
+  const requests = favoriteSeries.map(({ imdbId }) => get({ imdbId }))
   const movies = await Promise.all(requests)
   return movies
 }
 
-const search = async (params?: STGetMoviesParams) => {
-  let queryParams = ''
-  if (params) {
-    queryParams = params.imdbId ? `i=${params.imdbId}` : `s=${params.title}`
-  }
+const get = async ({ imdbId, title }: STGetParams): Promise<DTMovie> => {
+  const by = imdbId ? `i=${imdbId}` : `t=${title}`
   const request = await fetch(
-    `http://www.omdbapi.com/?apikey=${process.env.NEXT_PUBLIC_OMDB_API_KEY}&r=json&${queryParams}`
+    `http://www.omdbapi.com/?apikey=${process.env.NEXT_PUBLIC_OMDB_API_KEY}&r=json&type=movie&${by}`
+  )
+  const movie = await request.json()
+  return movie
+}
+
+const search = async (
+  type: 'movie' | 'series' | 'episode',
+  text?: string
+): Promise<STSearchReturn> => {
+  const request = await fetch(
+    `http://www.omdbapi.com/?apikey=${process.env.NEXT_PUBLIC_OMDB_API_KEY}&r=json&type=${type}&s=${text}`
   )
   const movies = await request.json()
   return movies
@@ -42,18 +59,15 @@ const top4 = async () => {
       imdbId: 'tt0133093',
       title: 'The Matrix',
     },
-    {
-      imdbId: 'tt0816692',
-      title: 'Interstellar',
-    },
   ]
-  const requests = topMovies.map(({ imdbId }) => search({ imdbId }))
+  const requests = topMovies.map(({ imdbId }) => get({ imdbId }))
   const movies = await Promise.all(requests)
   return movies
 }
 
 export const movies = {
   favorites,
+  get,
   search,
   top4,
 }
--- services/movies/movies.types.ts
+++ services/movies/movies.types.ts
@@ -1,4 +1,41 @@
-export type STGetMoviesParams = {
+export interface DTRating {
+  Source: string
+  Value: string
+}
+
+export type DTMovie = {
+  Actors: string
+  Awards: string
+  BoxOffice: string
+  Country: string
+  Director: string
+  DVD: string
+  Genre: string
+  imdbID: string
+  imdbRating: string
+  imdbVotes: string
+  Language: string
+  Metascore: string
+  Plot: string
+  Poster: string
+  Production: string
+  Rated: string
+  Ratings: DTRating[]
+  Released: string
+  Response: string
+  Runtime: string
+  Title: string
+  Type: string
+  Website: string
+  Writer: string
+  Year: string
+}
+
+export type STGetParams = {
   imdbId?: string
   title?: string
 }
+
+export type STSearchReturn = DTMovie | DTMovie[]
+
+export type STFavoritesReturn = DTMovie[]
