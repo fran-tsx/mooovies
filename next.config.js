fcd2f4a4f07bf3b4b56298f8ab486322efeea101
Francisco Rodriguez <fjrodriguez.353@icloud.com>
2023-02-02 16:47:17 -0300

    feat: next config updated

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/search',
+        permanent: true,
+      },
+    ]
+  },
+  compiler: {
+    styledComponents: true
+  },
   reactStrictMode: true,
-  styledComponents: true
 }
 
 module.exports = nextConfig
--- styled.d.ts
+++ styled.d.ts
@@ -4,7 +4,15 @@ import { theme } from './configs/theme'
 declare module 'styled-components' {
   export interface DefaultTheme {
     color: {
+      accent: '#E53935'
       black: '#202020'
+      gray: {
+        dark: '#616161'
+        light: '#E0E0E0'
+        normal: '#BDBDBD'
+        superlight: '#EEEEEE'
+      }
+      white: '#FFFFFF'
     }
   }
 }
