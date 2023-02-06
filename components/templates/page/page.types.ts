4ace6ba8af780264c08d9dd8524cd4a4db75287e
Francisco Rodriguez <fjrodriguez.353@icloud.com>
2023-02-06 09:50:15 -0300

    fix: unused import

--- components/templates/page/page.types.ts
+++ components/templates/page/page.types.ts
@@ -1,6 +1,5 @@
 import { CTSuggestions } from '@molecules/suggestions/suggestions.types'
 import { ReactNode } from 'react'
-import { DTMovie } from 'services/movies/movies.types'
 
 export type CTPage = {
   children: ReactNode
